1721129c898faf5fabaa6ff12f6101050b5616a8
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2026-02-12 21:13:12 +1100

    ci/cd: fix pathing in convert script

--- scripts/convert.sh
+++ scripts/convert.sh
@@ -1,18 +1,17 @@
 #!/bin/env bash
 
-CURRENT_DIR=$(dirname "$(realpath "$0")")
-# CALLED_FROM=$(pwd)
-# ROOT_DIR=${CURRENT_DIR}/..
-
-ROOT_DIR=$(realpath "${CURRENT_DIR}/..")
+# CURRENT_DIR=$(dirname "$(realpath "$0")")
+SCRIPTS_DIR=$(dirname "$(realpath "$0")")
+SCRIPTS_PARENT_DIR=$(dirname "${SCRIPTS_DIR}")
 
 ## To pull them down locally (for Typst)
 # wget https://github-readme-stats.vercel.app/api?username=mrdwarf7&show_icons=true&theme=dark#gh-dark-mode-only -o ./cards/stats.svg
 # wget "https://github-readme-stats.vercel.app/api/top-langs/?username=mrdwarf7&theme=dark&layout=compact&hide=powershell,lua#gh-dark-mode-only" -o ./cards/top-langs.svg
 
-printf "Current directory: %s\n" "${CURRENT_DIR}"
-printf "Root directory: %s\n" "${ROOT_DIR}"
+printf "Current directory: %s\n" "${SCRIPTS_DIR}"
+printf "Parent directory: %s\n" "${SCRIPTS_PARENT_DIR}"
 
-pandoc --pdf-engine=typst "${ROOT_DIR}/README.template.typ" -o "${ROOT_DIR}/README.md"
-data=$(cat "${ROOT_DIR}/README.md")
-echo "$data" | tr -d '`' | cat - | tee >"${ROOT_DIR}/README.md"
+pandoc --pdf-engine=typst "${SCRIPTS_PARENT_DIR}/README.template.typ" -o "${SCRIPTS_PARENT_DIR}/README.md"
+
+data=$(cat "${SCRIPTS_PARENT_DIR}/README.md")
+echo "$data" | tr -d '`' | cat - | tee >"${SCRIPTS_PARENT_DIR}/README.md"
